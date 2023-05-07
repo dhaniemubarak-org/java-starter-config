e855a5a05ea71444b10745fd164da7b45f42e526
Mochamad Ramdhanie Mubarak <dhaniemubarak@gmail.com>
2023-05-07 14:11:00 +0700

    Add find by phone number

--- src/main/java/id/alto/javastarterconfig/repository/SampleDbRepository.java
+++ src/main/java/id/alto/javastarterconfig/repository/SampleDbRepository.java
@@ -3,7 +3,10 @@ package id.alto.javastarterconfig.repository;
 import id.alto.javastarterconfig.entity.SampleDb;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SampleDbRepository extends CrudRepository<SampleDb, UUID> {
+
+    Optional<SampleDb> findByPhoneNumber(String phoneNumber);
 }
