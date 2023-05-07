1fd6edc07489308ffed0823bc641017fbaec0ba0
Mochamad Ramdhanie Mubarak <dhaniemubarak@gmail.com>
2023-05-07 12:56:19 +0700

    Init commit

---
+++ src/main/java/id/alto/javastarterconfig/JavaStarterConfigApplication.java
@@ -0,0 +1,13 @@
+package id.alto.javastarterconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStarterConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStarterConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/alto/javastarterconfig/JavaStarterConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package id.alto.javastarterconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStarterConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
